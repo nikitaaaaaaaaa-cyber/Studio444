bb4ee25d48b1e4e40c7a4faf5044597caa460894
nikitaaaaaaaaa-cyber <nikitacozma332@gmail.com>
2025-03-16 14:49:49 +0300

    Add files via upload

---
+++ js/animation.js
@@ -0,0 +1,95 @@
+let contentVue1 = document.querySelector('.price_anim_1');
+let button1 = document.querySelector('.price_anim_1');
+
+button1.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue1.classList.toggle("price_active");
+});
+
+let contentVue2 = document.querySelector('.price_anim_2');
+let button2 = document.querySelector('.price_anim_2');
+
+button2.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue2.classList.toggle("price_active");
+});
+
+let contentVue3 = document.querySelector('.price_anim_3');
+let button3 = document.querySelector('.price_anim_3');
+
+button3.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue3.classList.toggle("price_active");
+});
+
+let contentVue4 = document.querySelector('.price_anim_1');
+let button4 = document.querySelector('.price_anim_2');
+
+button4.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue4.classList.toggle("price_none");
+});
+
+let contentVue5 = document.querySelector('.price_anim_1');
+let button5 = document.querySelector('.price_anim_3');
+
+button5.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue5.classList.toggle("price_none");
+});
+
+let contentVue6 = document.querySelector('.price_anim_2');
+let button6 = document.querySelector('.price_anim_3');
+
+button6.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue6.classList.toggle("price_none");
+});
+
+let contentVue7 = document.querySelector('.price_anim_3');
+let button7 = document.querySelector('.price_anim_1');
+
+button7.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue7.classList.toggle("price_none");
+});
+
+let contentVue8 = document.querySelector('.price_anim_3');
+let button8 = document.querySelector('.price_anim_2');
+
+button8.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue8.classList.toggle("price_none");
+});
+
+let contentVue9 = document.querySelector('.fimg');
+let button9 = document.querySelector('.price_anim_1');
+
+button9.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue9.classList.toggle("img_none");
+});
+
+let contentVue10 = document.querySelector('.fimg-2');
+let button10 = document.querySelector('.price_anim_2');
+
+button10.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue10.classList.toggle("img_none");
+});
+
+let contentVue11 = document.querySelector('.fimg-3');
+let button11 = document.querySelector('.price_anim_3');
+
+button11.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue11.classList.toggle("img_none");
+});
+
+let contentVue12 = document.querySelector('.price_anim_2');
+let button12 = document.querySelector('.price_anim_1');
+
+button12.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    contentVue12.classList.toggle("price_none");
+});
